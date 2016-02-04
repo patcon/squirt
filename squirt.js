7773e55c0fef47939d7e3f20dc741192e2584c4f
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-02-04 13:59:07 -0800

    Don't count unspaced acronym letters as individual words.

--- squirt.js
+++ squirt.js
@@ -240,7 +240,7 @@ sq.host =  window.location.search.match('sq-dev') ?
     return function textToNodes(text) {
       text = "3\n 2\n 1\n " + text.trim('\n').replace(/\s+\n/g,'\n');
       return text
-             .replace(/[\,\.\!\:\;](?![\"\'\)\]\}])/g, "$& ")
+             .replace(/[\,\.\!\:\;](?![\"\'\)\]\}\w])/g, "$& ")
              .split(/[\s]+/g)
              .filter(function(word){ return word.length; })
              .map(wordToNode);
